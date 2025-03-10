b9e9eac807e2bce91a84263279ae10c247845a8d
unknown <YY213@ccs.local>
2025-03-10 20:47:17 +0800

    JavaScript Fundamentals

---
+++ script.js
@@ -0,0 +1,7 @@
+function filterEvenNumbers(numbers) {
+    return numbers.filter(num => num % 2 === 0);
+  }
+  
+  // Example usage:
+  console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); // Output: [2, 4, 6]
+  
